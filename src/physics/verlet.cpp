18ce5904126c6bdd57eb6b697c36fb28f486f97f
SuprimOjha <suprimojha25@gmail.com>
2026-09-02 16:07:14 +0545

    updated vertlet.cpp for neew simulation feature

--- src/physics/verlet.cpp
+++ src/physics/verlet.cpp
@@ -1,56 +1,34 @@
 #include "solarium/physics/verlet.hpp"
 
 namespace solarium::physics {
+    void VelocityVertlet::updatePosition(
+        celestial::CelestialBody& body,
+        double detaTime
+    )
+    {
+        const math::Vec3 position = body.position();
+        const math::Vec3 velocity = body.velocity();
+        const math::Vec3 acceleration = body.acceleration();
+        const math::Vec3 newPosition = position + velocity * deltaTime + acceleration * ( 0.5 * deltaTime * deltaTime );
+        
+        body.setPosition(
+            newPosition 
+        );
+      }
 
-void VelocityVerlet::integrate(
-    celestial::CelestialBody& body,
-    const math::Vec3& acceleration,
-    double deltaTime
-) {
-
-    const math::Vec3 oldPosition =
-        body.position();
-
-    const math::Vec3 oldVelocity =
-        body.velocity();
-
-    /*
-     * x(t + dt)
-     *
-     * x = x + v*dt + 1/2*a*dt²
-     */
-
-    const math::Vec3 newPosition =
-        oldPosition +
-        oldVelocity * deltaTime +
-        acceleration *
-            (
-                0.5 *
-                deltaTime *
-                deltaTime
+      void VelocityVerlet::updateVelocity(
+        celestial::CelestialBody& body,
+        const math::Vec3& newAcceleration,
+        double deltaTime
+          ){
+            const math::Vec3 velocity = body.velocity();
+            const math::Vec3 oldAcceleration = body.acceleration();
+            const math::Vec3 newVelocity = velocity + ( oldAcceleration + newAcceleration ) * (0.5 * deltaTime);
+            body.setVelocity(
+                newVelocity
             );
-
-    /*
-     * This is the first half of the
-     * Velocity-Verlet velocity update.
-     *
-     * The N-body solver recalculates
-     * acceleration after the position
-     * update.
-     */
-
-    const math::Vec3 newVelocity =
-        oldVelocity +
-        acceleration *
-            deltaTime;
-
-    body.setPosition(
-        newPosition
-    );
-
-    body.setVelocity(
-        newVelocity
-    );
+            body.setAcceleration(
+                newAcceleration
+            );
+          }
 }
-
-} // namespace solarium::physics

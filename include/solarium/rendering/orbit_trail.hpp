f0bc0d84d5bfa96dd39d6032bc8f8063bebd2b96
SuprimOjha <suprimojha25@gmail.com>
2026-09-04 20:47:54 +0545

    implementing orbital lines to be visible

--- include/solarium/rendering/orbit_trail.hpp
+++ include/solarium/rendering/orbit_trail.hpp
@@ -9,39 +9,24 @@ namespace solarium::rendering {
 
 class OrbitTrail {
 public:
-    explicit OrbitTrail(
-        std::size_t maxPoints = 2'000
-    );
+    explicit OrbitTrail(std::size_t maxPoints = 2000);
 
-    ~OrbitTrail();
-
-    OrbitTrail(const OrbitTrail&) = delete;
-    OrbitTrail& operator=(const OrbitTrail&) = delete;
-
-    void addPoint(
-        const math::Vec3& point
-    );
+    void addPoint(const math::Vec3& point);
 
     void clear();
 
-    void upload();
-
-    void draw() const;
+    [[nodiscard]]
+    const std::vector<math::Vec3>& points() const noexcept;
 
     [[nodiscard]]
     std::size_t size() const noexcept;
 
+    [[nodiscard]]
+    std::size_t maxPoints() const noexcept;
+
 private:
     std::vector<math::Vec3> points_;
-
     std::size_t maxPoints_;
-
-    unsigned int vao_;
-    unsigned int vbo_;
-
-    bool dirty_;
-
-    void initializeOpenGL();
 };
 
 }
